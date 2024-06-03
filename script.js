4abd74170c8ff39e7464034bdc5598840937900c
Sahilh1212 <sahilhirani01|@gmail.com>
2024-06-03 17:47:47 +0530

    final

---
+++ script.js
@@ -0,0 +1,30 @@
+const endDate = '12 December 2024 12:00 AM'
+
+const inputs = document.querySelectorAll('input')
+const date = document.querySelector('.date')
+
+date.innerText = endDate
+
+
+function clock() {
+    const end = new Date(endDate)
+    const now = new Date()
+    const diff = (end - now) / 1000;
+
+    if (diff < 0) return;
+
+    inputs[0].value = Math.floor(diff / 3600 / 24);
+    inputs[1].value = Math.floor(diff / 3600) % 24;
+    inputs[2].value = Math.floor(diff / 60) % 60;
+    inputs[3].value = Math.floor(diff) % 60;
+}
+
+clock()
+
+
+setInterval(
+    () => {
+        clock()
+    },
+    1000
+)
